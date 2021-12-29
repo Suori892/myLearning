4c9d22f77154123badf6860b8b635eb2e526c947
Starpersky <dkolomoets8@gmail.com>
2021-12-29 17:01:57 +0200

    My websites

---
+++ websites/Folly/app.js
@@ -0,0 +1,6 @@
+// var randomString = ''
+// var alphabet = 'qwertyuiop'
+// while (randomString < 10) {
+//   var randomString = alphabet[Math.floor(Math.random() * alphabet.length)]
+// }
+// alert(randomString)
